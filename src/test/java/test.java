ab116fc975343dd0932c38c387913f6960f73f52
Dweik97 <mdweik97@gmail.com>
2022-07-25 13:48:44 +0100

    Minesweeper Test

---
+++ src/test/java/test.java
@@ -0,0 +1,15 @@
+import MS.Code.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+public class test {
+
+    @Test
+    void testNoMines(){
+
+        assertEquals(12, Solution.solve(5,5,0.5));
+    }
+    }
+
+
+
